09dedb43d214b7bf6e79e68ded8a9e6706c15b1d
ptoliagithub <149826981+ptoliagithub@users.noreply.github.com>
2023-11-08 15:38:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,10 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button ('View Our Fruit List - add Your Fav'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])    
       back_from_function = insert_row_snowflake(add_my_fruit)
+      my_data_rows = get_fruit_load_list()
       my_cnx.close()
       streamlit.text(back_from_function)
+      streamlit.dataframe(my_data_rows)
 
 
 
